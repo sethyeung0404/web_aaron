90f6914abd6c3ae52c539d08294db8b42580d4ee
Seth <info@mykpi.com>
2022-01-12 18:59:13 +0800

    first commit

---
+++ src/views/IndexHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// core components
+import background from 'assets/img/background.mp4';
+
+function IndexHeader() {
+  return (
+    <>
+      <div className='background-image'>
+        <div className='space'/>
+        <video className='video' autoPlay loop muted>
+          <source src={background} type='video/mp4' />
+        </video>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
