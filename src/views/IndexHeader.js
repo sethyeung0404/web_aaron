72e233142ef2d97c48f7306a55739f75361fd562
Seth <info@mykpi.com>
2022-01-31 17:23:57 +0800

    iOS bug?

--- src/views/IndexHeader.js
+++ src/views/IndexHeader.js
@@ -10,7 +10,7 @@ function IndexHeader() {
       <div className='background-image'>
         <div className='space' />
         <div className='video-mask'>
-          <video className='video' autoPlay loop muted>
+          <video className='video' autoPlay loop muted playsinline="true" disablePictureInPicture="true">
             <source src={background} type='video/mp4' />
           </video>
         </div>
